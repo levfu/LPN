c5c0d4a03ea8c5c02929191d3ed662c2ad57c1c5
Nam-newbie <namvip482006@gmail.com>
2025-03-26 10:17:33 +0700

    Cập nhật chức năng Alert

---
+++ Library/src/main/java/com/example/library/alert/LibraryAlert.java
@@ -0,0 +1,51 @@
+package com.example.library.alert;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+
+import java.util.Optional;
+
+public class LibraryAlert {
+
+    public static void showSuccessAlert(String title, String message) {
+              Alert alert = new Alert(AlertType.INFORMATION);
+              alert.setTitle(title);
+              alert.setHeaderText(null);
+              alert.setContentText(message);
+              alert.showAndWait();
+    }
+
+    public static void showErrorAlert(String title, String message) {
+           Alert alert = new Alert(AlertType.ERROR);
+           alert.setTitle(title);
+           alert.setHeaderText(null);
+           alert.setContentText(message);
+           alert.showAndWait();
+    }
+
+    public static void showWarningAlert(String title, String message) {
+        Alert alert = new Alert(AlertType.WARNING);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    public static boolean showConfirmationAlert(String title, String message) {
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+
+        Optional<ButtonType> result = alert.showAndWait();
+        return result.isPresent() && result.get() == ButtonType.OK;
+    }
+
+    public static void showInformationAlert(String title, String message) {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+}
