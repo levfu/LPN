c29ce74586618cd527faa1d55252df99b11c4aed
Nam-newbie <namvip482006@gmail.com>
2025-04-18 17:05:00 +0700

    Thêm chức nnawg đánh giá sách

--- Library/src/main/java/com/example/library/bookmanagement/DatabaseHelper.java
+++ Library/src/main/java/com/example/library/bookmanagement/DatabaseHelper.java
@@ -19,13 +19,22 @@ public class DatabaseHelper {
                 + "author TEXT, "
                 + "dueDate TEXT"
                 + ");";
+        String ratingsql = "CREATE TABLE IF NOT EXISTS book_ratings ("
+                + "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + "isbn TEXT NOT NULL, "
+                + "rating INTEGER NOT NULL CHECK (rating >= 1 AND rating <= 5), "
+                + "comment TEXT, "
+                + "FOREIGN KEY (isbn) REFERENCES borrowed_books(isbn)"
+                + ")";
         try (Connection connection = connect();
              Statement stmt = connection.createStatement()) {
             stmt.execute(sql);
+            stmt.execute(ratingsql);
         } catch (SQLException e) {
             System.out.println("Lỗi khi tạo bảng: " + e.getMessage());
         }
     }
+
 
     public static void saveToDatabase(BookData book) {
            String sql = "INSERT INTO borrowed_books(title, author, isbn, dueDate) VALUES (?, ?, ?, ?)";
@@ -41,6 +50,21 @@ public class DatabaseHelper {
                e.printStackTrace();
            }
     }
+
+    public static void saveRating(String isbn, int rating, String comment) {
+           String ratingsql = "INSERT INTO book_ratings(isbn, rating, comment) VALUES (?, ?, ?)";
+           try (Connection connection = connect();
+           PreparedStatement pstmt = connection.prepareStatement(ratingsql)) {
+               pstmt.setString(1, isbn);
+               pstmt.setInt(2, rating);
+               pstmt.setString(3, comment);
+               pstmt.executeUpdate();
+           } catch (SQLException e) {
+               System.err.println("Lỗi khi lưu đánh giá: ");
+               e.printStackTrace();
+           }
+    }
+
     public static List<BookData> getAllBooks() {
         List<BookData> books = new ArrayList<>();
         String sql = "SELECT * FROM borrowed_books";
@@ -78,5 +102,28 @@ public class DatabaseHelper {
         } catch (SQLException e) {
             System.out.println("Loi khi xoa toan bo du lieu: " + e.getMessage());
         }
+    }
+
+    public static double getAverageRating(String isbn) {
+        double averageRating = 0;
+        int totalRatings = 0;
+        String sql = "SELECT rating FROM book_ratings WHERE isbn = ?";
+        try (Connection connection = connect();
+            PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setString(1, isbn);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                int sumRatings = 0;
+                while (rs.next()) {
+                    sumRatings += rs.getInt("rating");
+                    totalRatings++;
+                }
+                if (totalRatings > 0) {
+                    averageRating = (double) sumRatings / totalRatings;
+                }
+            }
+        }catch (SQLException e) {
+            System.out.println("Lỗi khi tính toán rating: " + e.getMessage());
+        }
+        return averageRating;
     }
 }
--- Library/src/main/java/com/example/library/bookmanagement/RatingBookController.java
+++ Library/src/main/java/com/example/library/bookmanagement/RatingBookController.java
@@ -1,9 +1,14 @@
 package com.example.library.bookmanagement;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+
+import java.security.PublicKey;
 
 public class RatingBookController {
     @FXML
@@ -15,14 +20,98 @@ public class RatingBookController {
     @FXML
     private Label authorLabel;
 
+    @FXML
+    private Label tagLabel;
+
+    @FXML
+    private Label descriptionLabel;
+
+    @FXML
+    private Label ratingLabel;
+
+    @FXML
+    private Button star1, star2, star3, star4, star5;
+
+    @FXML
+    private TextArea commentTextArea;
+
+    @FXML
+    private Button submitRatingButton;
+
+    private int rating = 0;
+    private Book currentBook;
+
     public void setBookInfo(Book book) {
+        currentBook = book;
         titleLabel.setText(book.getTitle());
-        authorLabel.setText(String.join(", ", book.getAuthor()));
+        authorLabel.setText("Authors: " + String.join(", ", book.getAuthor()));
+        tagLabel.setText("Tags: " + String.join(" ,", book.getCategory()));
+        descriptionLabel.setText("Description: " + book.getDescription());
         try {
             Image img = new Image(book.getThumbnail(), true);
             coverImage.setImage(img);
         } catch (Exception e) {
             System.out.println("Không thể load ảnh: " + e.getMessage());
         }
+        double avg = DatabaseHelper.getAverageRating(book.getIsbn());
+        ratingLabel.setText("Rating: " + String.format("%.2f", avg));
+    }
+
+    private void colorStars(int count) {
+        Button[] stars = {star1, star2, star3, star4, star5};
+        for (int i = 0; i < count; i++) {
+            stars[i].setStyle("-fx-font-size: 30px; -fx-background-color: white; -fx-text-fill:gold;");
+        }
+    }
+
+    private void resetStarColors() {
+        Button[] stars = {star1, star2, star3, star4, star5};
+        for (Button star : stars) {
+            star.setStyle("-fx-font-size: 30px; -fx-background-color: while;-fx-text-fill: gray;");
+        }
+    }
+
+    public void handleStarSelection(ActionEvent event) {
+        resetStarColors();
+        Button clickedStar = (Button) event.getSource();
+
+        if(clickedStar == star1) {
+            rating = 1;
+            colorStars(1);
+        } else if (clickedStar == star2) {
+            rating = 2;
+            colorStars(2);
+        } else if (clickedStar == star3) {
+            rating = 3;
+            colorStars(3);
+        } else if (clickedStar == star4) {
+            rating = 4;
+            colorStars(4);
+        } else if (clickedStar == star5) {
+            rating = 5;
+            colorStars(5);
+        }
+    }
+
+    @FXML
+    public void handleSubmitRating(ActionEvent event) {
+        if (currentBook == null || rating == 0) {
+            System.out.println("Vui lòng chọn sách và số sao!");
+            return;
+        }
+
+        String comment = commentTextArea.getText();
+        String isbn = currentBook.getIsbn();
+
+        DatabaseHelper.saveRating(isbn, rating, comment);
+
+        System.out.println("Cảm ơn bạn đã phản hồi.");
+
+        double avg = DatabaseHelper.getAverageRating(isbn);
+        ratingLabel.setText("Rating: " + String.format("%.2f", avg));
+
+        commentTextArea.clear();
+        resetStarColors();
+        rating = 0;
     }
 }
