2f9e8e55f70bbe5642bd7f890ef987f579c29d77
Nguyễn Hải Nam <namvip482006@Gmail.com>
2025-04-30 10:05:17 +0700

    Them chi tiet sach o search

--- BTL OOP/LoginRegister/LoginAndRegister/src/main/java/Books/DatabaseHelper.java
+++ BTL OOP/LoginRegister/LoginAndRegister/src/main/java/Books/DatabaseHelper.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DatabaseHelper {
-    private static final String DB_URL = "jdbc:sqlite:D:\\HL_OOP\\LPN\\BTL OOP\\borrowed_books.db";
+    private static final String DB_URL = "jdbc:sqlite:C:\\Users\\Admin\\Documents\\GitHub\\LPN\\BTL OOP\\borrowed_books.db";
     public static Connection connect() throws SQLException {
         return DriverManager.getConnection(DB_URL);
     }
